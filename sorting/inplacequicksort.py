af9b56d3a53f0d0c607c4bafef33039afe1c1363
denivyruck <denivyruck@gmail.com>
2017-03-08 23:51:46 -0300

    Inplace Quicksort

--- sorting/inplacequicksort.py
+++ sorting/inplacequicksort.py
@@ -1,12 +1,29 @@
 
+#a[:i] stores the lowest of the items
 def partition(a, p, r):
 	pivot = a[r]
-	i = p-1
+	i = p
 
-	for i in range(p, r-1):
-		if a[i] < pivot:
-			a[i], 
+	for j in range(p, r):
+		if a[j] < pivot:
+			a[i], a[j] = a[j], a[i]
+			i += 1
+	a[i], a[r] = a[r], a[i]
+	
+	return i
 
 def quicksort(a, p, r):
 	if p < r:
 		q = partition(a, p, r)
+		quicksort(a, p, q-1)
+		quicksort(a, q+1, r)
+
+def main():
+	a = [1, 77, 2, -1, -2, -4, 666, 1, 42]
+	quicksort(a, 0, len(a)-1)
+	print(a)
+
+	return 0
+
+if __name__ == '__main__':
+	main()
